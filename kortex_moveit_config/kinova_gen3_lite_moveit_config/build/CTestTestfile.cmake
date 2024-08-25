4d5327d4f7a41ffd372f25b12f0e0672c36fb5a3
blu-y <a_o@kakao.com>
2024-08-25 23:52:20 +0900

    Seperate group

---
+++ kortex_moveit_config/kinova_gen3_lite_moveit_config/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jet/ws_moveit2/src/ros2_kortex_jetson/kortex_moveit_config/kinova_gen3_lite_moveit_config
+# Build directory: /home/jet/ws_moveit2/src/ros2_kortex_jetson/kortex_moveit_config/kinova_gen3_lite_moveit_config/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
